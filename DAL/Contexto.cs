71c840bf0db044e528f5533705ef41b1d977b6f4
Sebastian <Sebastian>
2024-11-18 19:22:50 -0400

    Modelo y contexto actualizados

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,17 +7,18 @@ namespace SebastianSuarez_AP1_P2.DAL
     {
         public Contexto(DbContextOptions<Contexto> Options) : base(Options) { }
 
-        public DbSet<Registros> Registro { get; set; }
+        public DbSet<Combos> Combo { get; set; }
+        public DbSet<CombosDetalle> combosDetalles { get; set; }
 
-       /*protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Deudor>().
-            HasData(new List<Deudor>() {
-                new Deudor(){DeudorId = 1, DeudorName= "Pedro" },
-                new Deudor(){DeudorId= 2, DeudorName = "Angel" },
-                new Deudor(){DeudorId= 3, DeudorName= "Diego"}
-            });
-        }*/
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             base.OnModelCreating(modelBuilder);
+             modelBuilder.Entity<Combo>().
+             HasData(new List<Combo>() {
+                 new Deudor(){DeudorId = 1, DeudorName= "Pedro" },
+                 new Deudor(){DeudorId= 2, DeudorName = "Angel" },
+                 new Deudor(){DeudorId= 3, DeudorName= "Diego"}
+             });
+         }*/
     }
 }
