20bed43dd4af6db8dfd8da1057ded86e0d921ea7
Sebastian Suarez <Juan_Suarez1@ucne.edu.do>
2024-11-23 22:20:25 -0400

    Creacion de las migraciones y la base de datos

---
+++ Migrations/20241124021728_Inicial.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SebastianSuarez_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Costo = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<int>(type: "int", nullable: false),
+                    Existencia = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Combo",
+                columns: table => new
+                {
+                    ComboId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Precio = table.Column<int>(type: "int", nullable: false),
+                    Vendido = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Combo", x => x.ComboId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComboDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ComboId = table.Column<int>(type: "int", nullable: false),
+                    ArticuloId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Costo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComboDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_ComboDetalle_Articulos_ArticuloId",
+                        column: x => x.ArticuloId,
+                        principalTable: "Articulos",
+                        principalColumn: "ArticuloId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComboDetalle_Combo_ComboId",
+                        column: x => x.ComboId,
+                        principalTable: "Combo",
+                        principalColumn: "ComboId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Articulos",
+                columns: new[] { "ArticuloId", "Costo", "Descripcion", "Existencia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, 700, "Mouse USB o PS2", 15, 800 },
+                    { 2, 7000, "Monitor 17''", 6, 7500 },
+                    { 3, 2000, "Teclado USB o PS2", 8, 2050 },
+                    { 4, 35000, "Tarjeta de video", 20, 38000 },
+                    { 5, 1500, "Memoria RAM", 50, 1800 },
+                    { 6, 3500, "Procesador", 15, 3000 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Combo",
+                columns: new[] { "ComboId", "Descripcion", "Fecha", "Precio", "Vendido" },
+                values: new object[,]
+                {
+                    { 1, "Combo Gama ultra", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, false },
+                    { 2, "Combo Gama alta", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, false },
+                    { 3, "Combo Gama media", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, false },
+                    { 4, "Combo Gama baja", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, false }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComboDetalle_ArticuloId",
+                table: "ComboDetalle",
+                column: "ArticuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComboDetalle_ComboId",
+                table: "ComboDetalle",
+                column: "ComboId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComboDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Articulos");
+
+            migrationBuilder.DropTable(
+                name: "Combo");
+        }
+    }
+}
